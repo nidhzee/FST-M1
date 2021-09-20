a5ddbee695c75c5835d3f2fcfe7947e322131e26
nidhzee <35516761+nidhzee@users.noreply.github.com>
2021-09-20 11:09:34 +0530

    Add files via upload

---
+++ Java/Activities/Activity1.java
@@ -0,0 +1,16 @@
+package activities;
+
+public class Activity1 {
+
+    public static void main(String[] args) {
+        Car toyota = new Car();
+        toyota.make = 2014;
+        toyota.color = "black";
+        toyota.transmission = "manual";
+    
+        toyota.displayCharacterstics();
+        toyota.accelerate();
+        toyota.brake();
+    }
+
+}
